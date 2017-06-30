fa9b671397e5cf5e4ef8e8814e5ceb9d86360500
Artem Vodyanitsky <artem.vodyanitsky@codeit.com.ua>
2017-06-30 18:49:37 +0300

    browser synk

--- browserSynk/gulpfile.js
+++ browserSynk/gulpfile.js
@@ -17,15 +17,15 @@ gulp.task('default', function() {
         proxy: "http://codeit.pro/casino/32334/review.html",
         rewriteRules: [
             {
-                match: /css\/style\.cr\.min\.css/g,
+                match: /css\/style\.cr\.min\.css/g,//TODO file to replace
                 fn: function (req, res, match) {
-                    return '/assets/style.cr.min.css';
+                    return '/assets/style.cr.min.css';//TODO file that replaces
                 }
             }
         ],
         serveStatic: [{
-                route: '/assets',
-                dir: 'assets'
+                route: '/assets',//TODO folder with files to replace
+                dir: 'assets'//TODO folder with files to replace
          }],
     });
 });
